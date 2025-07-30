e295b93f91140c15e717807cba75080cb5ba6ca1
root <jaldabirdangol082@gmail.com>
2025-07-30 12:34:13 +0545

    v0.1.5

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['lh3.googleusercontent.com','cdn-icons-png.flaticon.com'],
+    domains: ['lh3.googleusercontent.com','cdn-icons-png.flaticon.com','res.cloudinary.com'],
     
   },
 };
--- src/components/uploadImage.tsx
+++ src/components/uploadImage.tsx
@@ -5,32 +5,42 @@ import { useState } from "react";
 
 export default function ImageUpload() {
   const [imageUrl, setImageUrl] = useState<string | null>(null);
+  console.log(imageUrl)
 
   return (
     <div className="flex flex-col items-center gap-4">
  
 
-<div className="relative w-[265px] h-[265px]">
-  {imageUrl ? (
-    <Image
-      src={imageUrl}
-      alt="Uploaded"
-      fill
-      className="object-contain rounded-lg shadow"
-    />
-  ) : (
-    <Image
-      src="/earbud.jpg"
-      alt="Uploaded"
-      fill
-      className="object-contain rounded-lg shadow"
-    />
-  )}
-</div>
+ <div className="relative w-[265px] h-[265px] rounded-lg shadow flex justify-center items-center bg-gray-100">
+        {imageUrl ? (
+          <Image
+            src={imageUrl}
+            alt="Uploaded"
+            fill
+            className="object-contain rounded-lg shadow"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-20 h-20 text-gray-400"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V7M3 7l9 6 9-6"
+            />
+          </svg>
+        )}
+      </div>
 
            <CldUploadWidget
         uploadPreset={process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET!}
-        onUpload={(result: any) => {
+        onSuccess={(result: any) => {
           setImageUrl(result.info.secure_url); 
           console.log("Uploaded Image URL:", result.info.secure_url);
         }}
@@ -40,7 +50,9 @@ export default function ImageUpload() {
             onClick={() => open?.()}
             className="px-4 py-2 bg-blue-600 text-white rounded-lg"
           >
-            Upload Image
+          {
+            imageUrl ? 'Change Image' : 'Upload Image'
+          }
           </button>
         )}
       </CldUploadWidget>
